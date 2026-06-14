414c2560f92b8ac91eb40586df2495920aee9c76
Rohitweb3 <rp5120523@gmail.com>
2026-06-14 16:07:58 +0530

    mongodb connected

---
+++ backend/src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+const app = express();
+import connectBD from "../config/database.js"
+
+import cookieParser from "cookie-parser";
+// import morgan from "morgan";
+
+// import cors from "cors";
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(morgan("dev"));
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+    
+// }));
+app.use(cookieParser());
+
+
+connectBD();
+
+
+export default app;
+
